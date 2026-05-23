3c42de10a39f345d34bf68fbe0c8c14e3b5b35e7
Chatterjay <320718257@qq.com>
2026-05-23 17:38:12 +0800

    客户端可选连接支持

--- src/main/java/org/chatterjay/emilink/Emilink.java
+++ src/main/java/org/chatterjay/emilink/Emilink.java
@@ -1,16 +1,18 @@
 package org.chatterjay.emilink;
 
 import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
+import net.minecraftforge.network.NetworkRegistry;
 import org.chatterjay.emilink.client.ModKeybindings;
 import org.chatterjay.emilink.network.NetworkHandler;
 import org.slf4j.Logger;
@@ -24,10 +26,21 @@ public class Emilink {
     public Emilink() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         modEventBus.addListener(this::commonSetup);
-        modEventBus.addListener(ModKeybindings::register);
+
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> modEventBus.addListener(ModKeybindings::register));
+
         MinecraftForge.EVENT_BUS.register(this);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
         NetworkHandler.register();
+
+        // 允许服务端安装本模组后，客户端可选择性安装、也可不安装直接连接
+        ModLoadingContext.get().registerExtensionPoint(
+                net.minecraftforge.fml.IExtensionPoint.DisplayTest.class,
+                () -> new net.minecraftforge.fml.IExtensionPoint.DisplayTest(
+                        () -> NetworkRegistry.IGNORESERVERONLY,
+                        (remoteVersion, isFromServer) -> true
+                )
+        );
     }
 
     private void commonSetup(final FMLCommonSetupEvent event) {
